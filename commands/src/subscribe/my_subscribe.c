57b842aa7981d22bc4a8f4d24c3d439d16417e8a
Yaowanart HURE <49785625+YaoHure@users.noreply.github.com>
2020-06-04 10:51:02 +0200

    Add files via upload

---
+++ commands/src/subscribe/my_subscribe.c
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2020
+** NWP_myteams_2019
+** File description:
+** my_subscribe
+*/
+
+#include "teams.h"
+
+//subscribe to the event of a team and its sub directories//
+// (enable reception of all events from a team) stock the data in BDD//
+
+void my_subscribe(t_teams *teams, __attribute__((unused))char **argv,
+__attribute__((unused))t_server *srt, __attribute__((unused))t_client *clt)
+{
+    dprintf(teams->cfd, "180 Subscribe work in progress.\r\n");
+}
---
+++ commands/src/subscribe/my_subscribed.c
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2020
+** NWP_myteams_2019
+** File description:
+** my_subscribed
+*/
+
+#include "teams.h"
+
+// list all subscribed teams or list all users subscribed to a team
+//need in the BDD
+
+void my_subscribed(t_teams *teams, __attribute__((unused))char **argv,
+__attribute__((unused))t_server *srt, __attribute__((unused))t_client *clt)
+{
+    dprintf(teams->cfd, "181 Subscribed work in progress.\r\n");
+}
