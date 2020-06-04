0f88fce4adf126dc143741e5bd6ba998e2b7f4d0
Yaowanart HURE <49785625+YaoHure@users.noreply.github.com>
2020-06-04 10:55:13 +0200

    Add files via upload

---
+++ database/include/bdd.h
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2020
+** my_teams_bdd
+** File description:
+** bdd
+*/
+
+#ifndef BDD_H_
+#define BDD_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include "../../server/include/server.h"
+
+#endif /* !BDD_H_ */
